b1235faae8fd8314daf4a36f04edb2a4fa24a1b3
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-06 18:38:59 -1000

    dog object

--- basics.js
+++ basics.js
@@ -13,18 +13,39 @@ var myName= 'Bryan';
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
- 
+ var canDrive= true;
+ 	if(person.age<16){
+ 	cantDrive= false;
+
+ 	}
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+function greet(name){
+	console.log("Hello, my name is " + name);
+}
+greet('Bryan');
+	// console.log(greet("bill"));
+	// greet(myName);
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+var dataTypes=['manoa', true, null, 13, [greet], undefined];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog={
+ 	name: 'Spot',
+ 	bark: function(){
+ 		console.log("my dogs name is " + dog.name);
+
+ 	}
+
+ };
+dog.bark();
